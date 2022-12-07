40787a1f7c8752d62d84725e8b30fc5dfb40d962
Marwane Ahansal <ahansalmarwane@gmail.com>
2022-12-07 08:16:18 +0100

    handle SIGUS1 and SIGUS2 signals

--- client.c
+++ client.c
@@ -6,19 +6,35 @@
 /*   By: mahansal <mahansal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 02:08:07 by mahansal          #+#    #+#             */
-/*   Updated: 2022/12/04 03:29:36 by mahansal         ###   ########.fr       */
+/*   Updated: 2022/12/07 08:11:01 by mahansal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
+#include <stdio.h>
+
+void	send_msg(pid_t server_pid, char *msg)
+{
+	kill(server_pid, SIGUSR2);
+	printf("msg to be sent: %s\n", msg);
+}
 
 int main(int argc, char *argv[])
 {
+	pid_t				server_pid;
+
 	if (argc != 3)
 	{
-		ft_putstr_fd(2, "Arguments are not enough!!\n");
-		exit(0);
+		ft_putstr_fd(2, "Programs takes only 2 arguments (Server PID and the message)!\n");
+		exit(1);
 	}
-	kill(atoi(argv[1]), 2);
+	//
+	// use ft_atoi
+	//
+	server_pid = atoi(argv[1]);
+	if (server_pid <= 0)
+		ft_putstr_fd(2, "Server PID is wrong!\n");
+	send_msg(server_pid, argv[2]);
+	// pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,27 +6,37 @@
 /*   By: mahansal <mahansal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 02:08:16 by mahansal          #+#    #+#             */
-/*   Updated: 2022/12/04 03:31:21 by mahansal         ###   ########.fr       */
+/*   Updated: 2022/12/07 08:15:52 by mahansal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 #include <stdio.h>
 
-void	handle(int n)
+void	signal_handler(int signal, siginfo_t *info, void *other)
 {
-	(void) n;
-	printf("Haaaa! you killed me\n");
+	printf("signale recived ==> %s\n", signal == SIGUSR1 ? "SIGUSER1" : "SIGUSER2");
+	
+	(void) other;
+	(void) info;
 }
+
 int main(void)
 {
-	pid_t	server_pid;
+	pid_t				server_pid;
+	struct	sigaction	s_sigaction;
 	
 	server_pid = getpid();
 	ft_putstr_fd(1, "Server PID is ==> ");
 	ft_putnbr_fd(1, server_pid);
 	ft_putchar_fd(1, '\n');
-	signal(SIGINT, handle);
+
+	
+	s_sigaction.sa_flags = SA_RESTART;
+	s_sigaction.sa_sigaction = signal_handler;
+	sigemptyset(&(s_sigaction.sa_mask));
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
 	pause();
 	return (0);
 }
