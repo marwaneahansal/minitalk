b2c9a19f95cba28f5901ac6d6cc8c5651952d87b
Marwane Ahansal <ahansalmarwane@gmail.com>
2022-12-22 06:11:57 +0100

    added bonus (the client acknowledge every message)

--- client.c
+++ client.c
@@ -6,13 +6,17 @@
 /*   By: mahansal <mahansal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 02:08:07 by mahansal          #+#    #+#             */
-/*   Updated: 2022/12/13 16:35:31 by mahansal         ###   ########.fr       */
+/*   Updated: 2022/12/22 05:38:11 by mahansal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 #include <stdio.h>
+#include <string.h>
 
+// 01100001
+// 01000000
+// 01000000
 void handle_char(int pid, char c)
 {
     int i;
@@ -28,7 +32,7 @@ void handle_char(int pid, char c)
             kill(pid, SIGUSR2);
         i++;
         mask = mask >> 1;
-				usleep(100);
+		usleep(100);
     }
 }
 
@@ -45,10 +49,33 @@ void	send_msg(pid_t server_pid, char *msg)
 	handle_char(server_pid, msg[i]);
 }
 
+typedef struct s_ss {
+	int len;
+	int recived_signals;
+} t_ss;
+
+t_ss *st;
+
+
+void	signal_handler()
+{
+	st->recived_signals++;
+	if (st->recived_signals == st->len * 8)
+	{
+		ft_putstr_fd(1, "\n Message sent successfully\n");
+		exit(1);
+	}
+	// (void) other;
+	// (void) info;
+}
+
 int main(int argc, char *argv[])
 {
 	pid_t				server_pid;
-
+	
+	st = malloc(sizeof(t_ss));
+	st->recived_signals = 0;
+	st->len = 0;
 	if (argc != 3)
 	{
 		ft_putstr_fd(2, "Program takes at least 2 arguments (Server PID and the message)!\n");
@@ -59,8 +86,25 @@ int main(int argc, char *argv[])
 	//*****************
 	server_pid = atoi(argv[1]);
 	if (server_pid <= 0)
-		ft_putstr_fd(2, "Server PID is wrong!\n");
+	{
+		ft_putstr_fd(2, "\033[1;31mServer PID is wrong !\033[0;0m\n");
+		exit(1);
+	}
+	struct	sigaction	s_sigaction;
+
+	s_sigaction.sa_flags = SA_NODEFER;
+	s_sigaction.sa_sigaction = signal_handler;
+	signal_handler();
+	sigemptyset(&(s_sigaction.sa_mask));
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	st->len = strlen(argv[2]);
 	send_msg(server_pid, argv[2]);
-	// pause();
+
+	
+	while (1)
+	{
+		sleep(1);
+	}
+	
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mahansal <mahansal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 02:08:16 by mahansal          #+#    #+#             */
-/*   Updated: 2022/12/13 16:38:12 by mahansal         ###   ########.fr       */
+/*   Updated: 2022/12/22 05:27:17 by mahansal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,21 @@ void	signal_handler(int signal, siginfo_t *info, void *other)
 {
 	static char c;
 	static int i;
+	// 00000000
+	// 01100001
 
 	if (signal == SIGUSR1)
+	{
+		// ft_putchar_fd(1, '1');
 		c = c | 1;
+		// kill(info->si_pid, SIGUSR1);
+	}
 	else if (signal == SIGUSR2)
+	{
+		// ft_putchar_fd(1, '0');
 		c = c | 0;
+		// kill(info->si_pid, SIGUSR1);
+	}
 	if (i < 7)
 		c = c << 1;
 	i++;
@@ -32,6 +42,7 @@ void	signal_handler(int signal, siginfo_t *info, void *other)
 		c = 0;
 		i = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
 	
 	(void) other;
 	(void) info;
@@ -43,15 +54,22 @@ int main(void)
 	struct	sigaction	s_sigaction;
 	
 	server_pid = getpid();
-	ft_putstr_fd(1, "Server PID is ==> ");
+	ft_putstr_fd(1, "\033[1;32mServer PID is \033[0;0m==> ");
 	ft_putnbr_fd(1, server_pid);
 	ft_putchar_fd(1, '\n');
 
 	s_sigaction.sa_flags = SA_NODEFER;
 	s_sigaction.sa_sigaction = signal_handler;
+	
 	sigemptyset(&(s_sigaction.sa_mask));
 	sigaction(SIGUSR1, &s_sigaction, 0);
 	sigaction(SIGUSR2, &s_sigaction, 0);
+
+	// signal(SIGUSR1, test);
+	// signal(SIGUSR2, test);
+
+
+	
 	while (1)
 		sleep(1);
 	return (0);
